ad72e0f362c395fa5d62cedf5c118732b7011939
Daniel J. Summers <daniel.summers.2415@gmail.com>
2020-04-04 15:51:47 -0500

    Update to Angular 9 (step 2)

--- bit-badger-solutions/src/app/applications/technology/technology.component.ts
+++ bit-badger-solutions/src/app/applications/technology/technology.component.ts
@@ -12,31 +12,33 @@ export class TechnologyComponent implements OnInit {
   @Input() tech: Technology
 
   /** External links to technology products */
-  private techLinks = {
-    'ASP.NET MVC': 'https://dotnet.microsoft.com/apps/aspnet/mvc',
-    Azure: 'https://azure.microsoft.com/',
-    'BlogEngine.NET': 'http://www.dotnetblogengine.net/',
-    'Database Abstraction': 'https://github.com/danieljsummers/DatabaseAbstraction',
-    'Digital Ocean': 'https://www.digitalocean.com/',
-    Giraffe: 'https://github.com/giraffe-fsharp/Giraffe',
-    GitHub: 'https://github.com/',
-    'GitHub Pages': 'https://pages.github.com/',
-    Hexo: 'https://hexo.io/',
-    Hugo: 'https://gohugo.io/',
-    Jekyll: 'https://jekyllrb.com/',
-    MongoDB: 'https://www.mongodb.com/',
-    MySQL: 'https://www.mysql.com/',
-    myWebLog: 'https://github.com/bit-badger/myWebLog',
-    nginx: 'http://nginx.org/',
-    Orchard: 'https://orchardproject.net/',
-    PHP: 'https://www.php.net/',
-    PostgreSQL: 'https://www.postgresql.org/',
-    'Rackspace Cloud': 'https://www.rackspace.com/cloud',
-    RavenDB: 'https://ravendb.net/',
-    RethinkDB: 'https://rethinkdb.com/',
-    'SQL Server': 'https://www.microsoft.com/en-us/sql-server/',
-    'Vue.js': 'https://vuejs.org/',
-    WordPress: 'https://wordpress.org'
+  get techLinks() {
+    return {
+      'ASP.NET MVC': 'https://dotnet.microsoft.com/apps/aspnet/mvc',
+      Azure: 'https://azure.microsoft.com/',
+      'BlogEngine.NET': 'http://www.dotnetblogengine.net/',
+      'Database Abstraction': 'https://github.com/danieljsummers/DatabaseAbstraction',
+      'Digital Ocean': 'https://www.digitalocean.com/',
+      Giraffe: 'https://github.com/giraffe-fsharp/Giraffe',
+      GitHub: 'https://github.com/',
+      'GitHub Pages': 'https://pages.github.com/',
+      Hexo: 'https://hexo.io/',
+      Hugo: 'https://gohugo.io/',
+      Jekyll: 'https://jekyllrb.com/',
+      MongoDB: 'https://www.mongodb.com/',
+      MySQL: 'https://www.mysql.com/',
+      myWebLog: 'https://github.com/bit-badger/myWebLog',
+      nginx: 'http://nginx.org/',
+      Orchard: 'https://orchardproject.net/',
+      PHP: 'https://www.php.net/',
+      PostgreSQL: 'https://www.postgresql.org/',
+      'Rackspace Cloud': 'https://www.rackspace.com/cloud',
+      RavenDB: 'https://ravendb.net/',
+      RethinkDB: 'https://rethinkdb.com/',
+      'SQL Server': 'https://www.microsoft.com/en-us/sql-server/',
+      'Vue.js': 'https://vuejs.org/',
+      WordPress: 'https://wordpress.org'
+    }
   }
 
   constructor() { }
